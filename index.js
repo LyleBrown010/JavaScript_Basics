c2849df704253dbb5577932a71736159b342e67a
LyleBrown010 <lylebrown010@gmail.com>
2023-05-16 09:15:50 +0200

    completed exercise for day 3

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function myFunction(){
     alert ("Hello! My name is Lyle !!")
 };
 
+// exercise 1.1 
+function myButton(){
+    alert ('this is the alert box')
+    console.log('Hi World')
+};
+
+
 // exercise 1.3 and 1.4 
 console.log ('Hello World'); 
 
@@ -40,7 +47,26 @@ console.log(gender);
 
 // exercise 2.3 / third part 
 let a, b, c; a=b=c=0; 
-c--; 
+c-- ; 
+
 document.write("The value of a is :", a + "<br/>"); 
 document.write("The value of a is :", b + "<br/>"); 
 document.write("The value of a is :", c + "<br/>"); 
+
+
+// exercise 3.2 
+const person2 = {
+    name3: 'John', 
+    age2: 30, 
+    isUser: true, 
+    firstName: undefined, 
+    selectedColor: null, 
+}; 
+
+let{name3, age2, isUser, firstName, selectedColor} = person2; 
+
+console.log(name3);
+console.log(age2);
+console.log(isUser);
+console.log(firstName);
+console.log(selectedColor);
